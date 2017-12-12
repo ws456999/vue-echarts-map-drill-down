40fcc5d9e5b246d6dbe2c5d5552c8bcba367bc86
ws456999 <ruiru5921@sina.com>
2017-12-12 17:57:49 +0800

    :tada: init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# vue-echarts-map-drill-down
+
+echarts map drill down in vue
+
+Echarts3 中国地图下钻
+
+## preview
+
+![drill down](./static/demo.gif)
+
+## run this repo
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:8039
+npm run dev
+
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+  width: 100%;
+}
+html, body {
+  height: 100%;
+  width: 100%;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
